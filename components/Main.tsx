10984a0642dcf37e597d5c6d4e1e6268bf87ef28
MaxBrych <max.brych03@gmail.com>
2023-02-12 10:46:17 +0100

    flex-none

--- components/Main.tsx
+++ components/Main.tsx
@@ -6,7 +6,7 @@ export const Main = () => {
   return (
     <main className="flex flex-col items-start justify-start flex-1 w-full h-full  max-w-5xl px-4 pt-6 text-center bg-white sm:mt-20">
       <h1 className="max-w-2xl text-2xl font-bold text-left sm:text-4xl text-slate-900">
-        Neuer Tag, neues Glück!
+        Willkommen zurück!
       </h1>
       <ToolList tools={tools} />
       <p className="mt-5 text-slate-500"></p>
